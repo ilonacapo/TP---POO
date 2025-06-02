2598b6afe7677caadc88d950bfde2308ba701443
ilonacapo <ilo@ILONA>
2025-06-02 12:02:27 +0200

    add produit

--- Produit/Produit.php
+++ Produit/Produit.php
@@ -1,21 +1,53 @@
 <?php
 
-class Produit {
+class Produit
+{
     private $nom;
     private $prix;
 
-    public function __construct(string $nom, float $prix) {
+    public function __construct(string $nom, float $prix)
+    {
         $this->nom = $nom;
         $this->prix = $prix;
     }
 
-public function getNom() {
-    return $this->nom;
-}
+    public function getNom()
+    {
+        return $this->nom;
+    }
 
-public function getPrix() {
-    return $this->prix;
-}
+    public function getPrix()
+    {
+        return $this->prix;
+    }
 
+    public function afficherProduit()
+    {
+        echo "Nom: $this->nom, Prix: $this->prix\n";
+    }
 
+    public function ajouterAuPanier(array $panier, int $quantite)
+    {
+        $index = 0;
+
+        if ($quantite > 0) {
+            while ($index < $quantite) {
+                array_push($panier, $this);
+                $index += 1;
+            }
+            return $panier;
+        }
+    }
+
+    public function showQuantity(array $panier, string $nomP) {
+        $nb = 0;
+        foreach ($panier as $produit) {
+            $nom = $produit->getNom();
+            if ($nom === $nomP) {
+                $nb += 1;
+            }
+
+        }
+        return $nb;
+    }
 }
--- Produit/index.php
+++ Produit/index.php
@@ -10,3 +10,28 @@ $prix_1 = $produit_1->getPrix();
 
 $nom_2 = $produit_2->getNom();
 $prix_2 = $produit_2->getPrix();
+
+$panier = [];
+array_push($panier, $produit_1);
+array_push($panier, $produit_2);
+
+foreach ($panier as $produit) {
+    $nom = $produit->getNom();
+    $prix = $produit->getPrix();
+    echo "Nom: $nom, Prix: $prix\n";
+}
+$prixTotal = 0;
+
+foreach ($panier as $produit) {
+    $prix = $produit->getPrix();
+    $prixTotal += $prix;
+}
+
+echo "Prix Total: $prixTotal\n";
+
+$panier = $produit_1->ajouterAuPanier($panier, 5);
+$qty_1 = $produit_1->showQuantity($panier, $nom_1);
+
+$qty_2 = $produit_2->showQuantity($panier, $nom_2);
+
+echo "Quantité de\n$nom_1: $qty_1\n$nom_2: $qty_2";
